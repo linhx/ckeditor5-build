c3d131b3af21a47bc2493d1fa97cb7a48ea78b85
linhx <nguyendinhlinhx@gmail.com>
2022-10-26 01:58:23 +0700

    feat: add emoji plugin

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -29,6 +29,7 @@ import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefrom
 import Table from '@ckeditor/ckeditor5-table/src/table';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
 import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation';
+import { Emoji } from '@linhx/ckeditor5-emoji';
 
 export default class ClassicEditor extends ClassicEditorBase {}
 
@@ -56,7 +57,8 @@ ClassicEditor.builtinPlugins = [
 	PasteFromOffice,
 	Table,
 	TableToolbar,
-	TextTransformation
+	TextTransformation,
+	Emoji
 ];
 
 // Editor configuration.
@@ -78,6 +80,8 @@ ClassicEditor.defaultConfig = {
 			'blockQuote',
 			'insertTable',
 			'mediaEmbed',
+			'emoji',
+			'|',
 			'undo',
 			'redo'
 		]
